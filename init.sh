083f7963d99712af5323733c0ed7910717b18117
Grant Walker <grant.v.walker@gmail.com>
2021-03-16 18:08:57 +0200

    Updated init.sh to use different origin

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ cd ~
 yay -S --noconfirm fzf tree bat diff-so-fancy
 git clone https://github.com/kazhala/dotbare.git ~/.dotbare
 source ~/.dotbare/dotbare.plugin.zsh
-dotbare finit -u https://github.com/gvwalker/dotfiles.git
+dotbare finit -u https://github.com/gvwalker/dotfiles
+dotbare remote set-url origin git@github.com:gvwalker/dotfiles.git
 ./setup.sh
