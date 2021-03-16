bf7fde085cce9dced32284e4f7927228a84b3fb3
Grant Walker <grant.v.walker@gmail.com>
2021-03-16 20:25:07 +0200

    Remove homebrew

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S --noconfirm bmenu bpytop neovim alacritty exa
+yay -S --noconfirm bmenu bpytop neovim alacritty exa starship-bin
 
 yay -S --noconfirm teams zoom telegram-desktop
 yay -S --noconfirm vivaldi vivaldi-ffmpeg-codecs vivaldi-widevine
@@ -12,17 +12,14 @@ sudo mkdir -p /vms/vm-storage && sudo chown grant:grant /vms/vm-storage
 sudo usermod -aG libvirt grant
 sudo systemctl enable libvirtd.service && sudo systemctl start libvirtd.service
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew tap homebrew/bundle
-brew bundle
-yay -S --noconfirm visual-studio-code-bin kind-bin kubectl helm docker nerd-fonts-source-code-pro
+yay -S --noconfirm github-cli-git terraform tflint tfsec
+yay -S --noconfirm visual-studio-code-bin nerd-fonts-source-code-pro
+git clone https://github.com/gvwalker/awscli-installer-linux awscli
+./awscli/install-awscli.sh
+fc-cache -f -v
+sudo timedatectl set-ntp true
+
+yay -S --noconfirm kind-bin kubectl helm docker
 sudo usermod -aG docker grant
 sudo systemctl enable docker.service && sudo systemctl start docker.service
 sudo kind create cluster
-
-git clone https://github.com/gvwalker/awscli-installer-linux awscli
-./awscli/install-awscli.sh
-
-fc-cache -f -v
-sudo timedatectl set-ntp true
