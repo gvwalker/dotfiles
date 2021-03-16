6030ca59d27e41329757730364cb6a80dfe24b54
Grant Walker <grant.v.walker@gmail.com>
2021-03-16 15:19:35 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~
-yay -Sy fzf tree bat diff-so-fancy
+yay -S --noconfirm fzf tree bat diff-so-fancy
 git clone https://github.com/kazhala/dotbare.git ~/.dotbare
 dotbare finit -u https://github.com/gvwalker/dotfiles.git
 ./setup.sh
