3569bafbb7669c0fb6d48736854943705a1f673b
Grant Walker <grant.v.walker@gmail.com>
2021-03-16 15:56:07 +0200

    Added setup script for AwesomeWM

---
+++ awesome-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -S --noconfirm awesome-git picom-tryone-git volumeicon unclutter blueman rofi-git lxappearance upower xorg-xbacklight polkit lxsession i3lock-multimonitor nitrogen
