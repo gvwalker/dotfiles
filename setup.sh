fb04429587f7fd31f47757e5edc91697115a1129
Grant Walker <grant.v.walker@gmail.com>
2021-03-26 13:16:31 +0200

    Changed setup script
    Using google-chrome
    Added unzip
    Fixed awscli install script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yay -S --noconfirm bmenu bpytop neovim alacritty exa starship-bin
+yay -S --noconfirm bmenu bpytop neovim alacritty exa unzip starship-bin
 
 yay -S --noconfirm teams zoom telegram-desktop
-yay -S --noconfirm vivaldi vivaldi-ffmpeg-codecs vivaldi-widevine
+yay -S --noconfirm google-chrome
 yay -S --noconfirm youtube-music-appimage
 
 yay -S --noconfirm qemu ebtables libvirt bridge-utils virt-manager virt-viewer
@@ -15,7 +15,7 @@ sudo systemctl enable libvirtd.service && sudo systemctl start libvirtd.service
 yay -S --noconfirm github-cli-git terraform tflint tfsec
 yay -S --noconfirm visual-studio-code-bin nerd-fonts-source-code-pro
 git clone https://github.com/gvwalker/awscli-installer-linux awscli
-./awscli/install-awscli.sh
+sudo ./awscli/install-cli
 fc-cache -f -v
 sudo timedatectl set-ntp true
 
