2fe8bc9ff14015a19d99bbfbebdd605e268a0f0e
Grant Walker <grant.v.walker@gmail.com>
2023-07-10 14:22:59 +0200

    Added AstroNvim Configuration

---
+++ .config/astronvim/lua/user/plugins/community.lua
@@ -0,0 +1,8 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+}
