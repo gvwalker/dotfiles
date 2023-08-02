39baaf85b2b81bcac37011fdf5090ce76f507aa1
Grant Walker <grant.v.walker@gmail.com>
2023-08-02 20:37:05 +0200

    Changed to Dracula theme

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "dracula",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
